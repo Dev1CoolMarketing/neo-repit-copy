b72ca125fb7ee855e7fbe245b7b3281a7063f0d7
Developer One <developer1@coolmarketing.com>
2025-10-23 08:10:45 -0700

    feat(update): update with vercel analytics

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,8 +3,15 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+import { Analytics } from '@vercel/analytics/react'   // ✅ add this
+
+
+
+
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+     <Analytics /> 
   </StrictMode>
 )
